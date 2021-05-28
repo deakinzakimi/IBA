b185fbb21d02b22a842d697c5380f5306f2d3b02
Michael Zaki <mike_tadrous@hotmail.com>
2021-05-29 03:46:49 +1000

    Upload database

---
+++ iba.sql
@@ -0,0 +1,85 @@
+-- --------------------------------------------------------
+-- Host:                         database10.c7klzuo1nogr.ap-southeast-2.rds.amazonaws.com
+-- Server version:               8.0.20 - Source distribution
+-- Server OS:                    Linux
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for iba
+DROP DATABASE IF EXISTS `iba`;
+CREATE DATABASE IF NOT EXISTS `iba` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `iba`;
+
+-- Dumping structure for table iba.interview
+DROP TABLE IF EXISTS `interview`;
+CREATE TABLE IF NOT EXISTS `interview` (
+  `iid` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `sid` char(10) NOT NULL,
+  `stid` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `istarttime` datetime NOT NULL,
+  `ifinishtime` datetime NOT NULL,
+  `izoomlink` varchar(100) DEFAULT NULL,
+  `istatus` varchar(50) NOT NULL,
+  `inote` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table iba.organizer
+DROP TABLE IF EXISTS `organizer`;
+CREATE TABLE IF NOT EXISTS `organizer` (
+  `oid` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `oactive` tinyint NOT NULL,
+  `ofirstname` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `olastname` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `oaddress1` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `oaddress2` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `osuburb` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `ostate` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `opostcode` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `ocountry` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `ophone` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `oemail` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `ousername` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `opassword` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`oid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table iba.slot
+DROP TABLE IF EXISTS `slot`;
+CREATE TABLE IF NOT EXISTS `slot` (
+  `sid` char(10) NOT NULL,
+  `orid` char(10) NOT NULL,
+  `sstarttime` datetime NOT NULL,
+  `sfinishtime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table iba.student
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `sid` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `sactive` tinyint NOT NULL,
+  `sfirstname` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `slastname` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `semail` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`sid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
